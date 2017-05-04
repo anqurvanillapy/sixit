182c30d642e9560a09ab804690c9664a6ed150ff
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-05 01:11:45 +0800

    added babel and vanilla test case

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-class sixit {}
+export default class sixit {
+  sixit (f) {
+    if (typeof f !== 'function') throw TypeError(`${f.name} is not a function`)
 
-export default { sixit }
+    console.time(f.name)
+    let ret = f()
+    console.timeEnd()
+    return ret
+  }
+}
--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-const sixit = require('sixit')
+import assert from 'assert'
+import sixit from './index.compiled.js'
+
+class TestSixit {
+  testFunctionElapsed () {
+    process.stdout.write('record function elapsed time...')
+  }
+}
+
+let tc = Reflect.ownKeys(TestSixit.prototype)
+let ntc = tc.length
+let passed = 0
+
+for (let i = 0; i < ntc; ++i) {
+  let m = tc[i]
+  if (m !== 'constructor') {
+    process.stdout.write('[test] ')
+    try {
+      TestSixit.prototype[m]()
+      console.log('️ \x1b[32m✔️\x1b[0m')
+      ++passed
+    } catch (err) {
+      console.log(' x1b[35m✖️\x1b[0m')
+    }
+  }
+}
+
+console.log(`\n${passed} / ${ntc - 1} passed...`) // excludes constructor
