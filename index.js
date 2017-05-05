de6f523194c2d87562efa41d23c3a2379d19dd0d
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-05 16:11:06 +0800

    added oneit, sixit

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-export default class sixit {
-  sixit (f) {
-    if (typeof f !== 'function') throw TypeError(`${f.name} is not a function`)
-
-    console.time(f.name)
-    let ret = f()
-    console.timeEnd()
-    return ret
-  }
+function preassert (f) {
+  if (typeof f !== 'function') throw TypeError(`${f.name} is not a function`)
 }
+
+function oneit (f, ...args) {
+  preassert(f)
+  let ret
+  let fname = f.name
+
+  console.time(fname)
+  ret = f(...args)
+  console.timeEnd(fname)
+  return ret
+}
+
+function sixit (f, ...args) {
+  preassert(f)
+  let ms = 0
+  let start, end
+  let ret = f(...args)
+
+  start = process.hrtime()
+  // It is fine, although not cool at all.
+  f(...args)
+  f(...args)
+  f(...args)
+  f(...args)
+  f(...args)
+  f(...args)
+  end = process.hrtime(start)
+  ms += end[0] * 1000.0 + end[1] / 1000000.0  // [sec, nanosec]
+
+  console.log(`${f.name}: ${ms / 6.0}ms (sixit)`)
+  return ret
+}
+
+export { oneit, sixit }
--- test.js
+++ test.js
@@ -1,9 +1,26 @@
 import assert from 'assert'
-import sixit from './index.js'
+import { oneit, sixit } from './index.js'
 
 class TestSixit {
-  testFunctionElapsed () {
-    process.stdout.write('record function elapsed time...')
+  testIterativeFunction () {
+    function sumIter (b, t) {
+      for (let i = 1; i <= t; ++i) b += i
+      return b
+    }
+
+    // XXX: Too much difference.
+    assert(oneit(sumIter, 0, 900) === 405450)
+    assert(sixit(sumIter, 0, 900) === 405450)
+  }
+
+  testRecursiveFunction () {
+    function sumRecur (b, t) {
+      if (t > 0) return sumRecur(b + t, t - 1)
+      else return b
+    }
+
+    assert(oneit(sumRecur, 0, 900) === 405450)
+    assert(sixit(sumRecur, 0, 900) === 405450)
   }
 }
 
@@ -11,18 +28,25 @@ let tc = Reflect.ownKeys(TestSixit.prototype)
 let ntc = tc.length
 let passed = 0
 
+process.stdout.write('\n')
+console.time('elapsed')
+
 for (let i = 0; i < ntc; ++i) {
   let m = tc[i]
+
   if (m !== 'constructor') {
-    process.stdout.write('[test] ')
+    process.stdout.write(`[test] #${i} ${m} ...`)
     try {
       TestSixit.prototype[m]()
       console.log('️ \x1b[32m✔️\x1b[0m')
       ++passed
     } catch (err) {
-      console.log(' x1b[35m✖️\x1b[0m')
+      console.error(err)
+      console.log(' \x1b[31m✖️\x1b[0m')
     }
   }
 }
 
-console.log(`\n${passed} / ${ntc - 1} passed...`) // excludes constructor
+process.stdout.write('\n')
+console.timeEnd('elapsed')
+console.log(`${passed} / ${ntc - 1} passed...`) // excludes constructor
